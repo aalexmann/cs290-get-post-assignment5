e95614ba122ddf66a405569b296b8ecdead1a0a0
Alex Hidalgo <55524595+aalexmann@users.noreply.github.com>
2021-02-23 00:09:16 -0800

    update script.js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 8080);
 
+var options = { root: __dirname + '/'}
 
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
@@ -20,49 +21,41 @@ app.use(function(req, res, next){
 
 app.get('/form', function(req, res){
     res.type('text/html');
-    res.sendFile('index.html', options);
+    res.sendFile('form.html', options);
 });
 
 var gottenObjects = function(object){
-    var ulOpeningTag = '<ul>';
+    var ulOpen = '<ul>';
     for (var key in object){
-        ulOpeningTag += '<li>' + key + ' ' + object[key] + '</li>';
+        ulOpen += '<li>' + key + ' ' + object[key] + '</li>';
     }
-    return ulOpeningTag + '</ul>';
+    return ulOpen + '</ul>';
 }
 
 var getRequestTable = function(req){}
 
 app.get('/', function(req, res){
-    var html = '<h1>GET Request Received</h1>';
-    html += '<table><tbody><tr>';
-    html += '<td>url: ' + req.url + '</td>';
-    html += '<td>body: ' + gottenObjects(req.body) + '</td>';
-    html += '</tr></tbody></table>';
+    var html = '<h1>POST Request Received</h1> <table><tbody><tr><td>url: '+ req.url + '</td><td>body: ' + gottenObjects(req.body) + '</td></tr></tbody></table>';
     res.render(html);
 });
 
 app.post('/', function(req, res){
-    var html = '<h1>POST Request Received</h1>';
-    html += '<table><tbody><tr>';
-    html += '<td>url: ' + req.url + '</td>';
-    html += '<td>body: ' + gottenObjects(req.body) + '</td>';
-    html += '</tr></tbody></table>';
+    var html = '<h1>POST Request Received</h1> <table><tbody><tr><td>url: '+ req.url + '</td><td>body: ' + gottenObjects(req.body) + '</td></tr></tbody></table>';
     res.render(html);
 });
 
 app.use(function(req,res){
     res.status(404);
-    res.render('404');
+    res.render('404 - File Could Not be Found');
   });
   
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.type('plain/text');
-    res.status(500);
-    res.render('500');
-  });
+//   app.use(function(err, req, res, next){
+//     console.error(err.stack);
+//     res.type('plain/text');
+//     res.status(500);
+//     res.render('500');
+//   });
   
-  app.listen(app.get('8080'), function(){
-    console.log('Express started on http://localhost:' + app.get('8080') + '; press Ctrl-C to terminate.');
+  app.listen(port, function(){
+    console.log('Express started on http://localhost:' + port + '; press Ctrl-C to terminate.');
   });
